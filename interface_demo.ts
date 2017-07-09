e08db72c56e9a837af9b5554eb673ef3384d5ee1
Anwesh43 <anweshthecool0@gmail.com>
2017-07-09 19:18:22 +0530

    adding index property in typecript

--- interface_demo.ts
+++ interface_demo.ts
@@ -7,3 +7,15 @@ function greet(p:Person){
 }
 var p:Person = {name:"Ank",age:26}
 greet(p)
+
+interface PersonArray {
+    [index:number]:Person;
+}
+
+function greetAllPersons(persons:PersonArray) {
+    let personArray = persons as Array<Person>
+    personArray.forEach((person:Person)=>{
+        greet(person)
+    })
+}
+greetAllPersons([{name:"An1",age:25},{name:"An2",age:26},{name:"An3",age:27},{name:"An4",age:28}])
