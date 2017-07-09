d0715d2cd769c40b8aae1284edc90fa12e81ecf3
Anwesh43 <anweshthecool0@gmail.com>
2017-07-09 19:11:38 +0530

    making properties of an interface readonly

--- interface_demo.ts
+++ interface_demo.ts
@@ -1,11 +1,9 @@
 interface Person {
-    name:string;
-    age:number;
+    readonly name:string;
+    readonly age:number;
 }
 function greet(p:Person){
     alert(`hello ${p.name} and age is ${p.age}`)
 }
 var p:Person = {name:"Ank",age:26}
-greet(p)
-p.age = 27
 greet(p)
