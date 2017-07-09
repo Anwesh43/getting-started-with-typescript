e114e6faae5bf5bc8ad8dc99384688d5f8ae0818
Anwesh43 <anweshthecool0@gmail.com>
2017-07-09 23:46:11 +0530

    adding enum demo

---
+++ enum_demo.ts
@@ -0,0 +1,12 @@
+enum Fruit {
+    APPLE,
+    MANGO,
+    BANANA,
+    ORANGE
+}
+console.log(Fruit)
+console.log(typeof(Fruit))
+alert(Fruit.APPLE)
+alert(Fruit.MANGO)
+alert(Fruit.BANANA)
+alert(Fruit.ORANGE)
