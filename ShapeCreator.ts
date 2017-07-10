6f37153519fbb2b9dc76bfa48205ca89d6335f6a
Anwesh43 <anweshthecool0@gmail.com>
2017-07-10 20:30:24 +0530

    Adding fixes for animation and drawing

--- ShapeCreator.ts
+++ ShapeCreator.ts
@@ -1,6 +1,7 @@
 const w = window.innerWidth,h = window.innerHeight
 class Shape  {
     div:any;
+    scale:number = 1
     constructor(public x:number,public y:number) {
     }
     define(color:string) {
@@ -14,13 +15,11 @@ class Shape  {
         document.body.appendChild(this.div)
     }
     update() {
-        if(parseFloat(this.div.style.width) > 0) {
-            this.div.style.width = parseFloat(this.div.style.width)- (w/50)
-            this.div.style.height = parseFloat(this.div.style.height)- (w/50)
-        }
+          this.div.style.transform = `scaleX(${this.scale})scaleY(${this.scale})`
+          this.scale -=0.1
     }
     stopped():boolean {
-        return this.div.style.width <= 0
+        return this.scale <= 0
     }
 }
 class Square extends Shape {
@@ -35,25 +34,38 @@ class Circle extends Shape {
 class Triangle extends Shape {
     define(color) {
         super.define(color)
-        const border = `${w/20}px solid transparent`
-        this.div.style.borderRight = border
-        this.div.style.borderLeft = border
-        this.div.style.borderBottom = `${w/10}px solid ${color}`
         this.div.style.width = 0
         this.div.style.height = 0
         this.div.style.background = 'transparent'
+        this.defineBorders()
+        this.div.style.borderBottomStyle = 'solid'
+        this.div.style.borderBottomColor = color
+    }
+    defineBorders() {
+        const border = `${w/20*this.scale}px solid transparent`
+        this.div.style.borderRight = border
+        this.div.style.borderLeft = border
+        this.div.style.borderBottomWidth = `${(w/10)*this.scale}px`
+    }
+    update() {
+        super.update()
+        this.defineBorders()
     }
 }
 class ShapeFactory {
-    static color:string = 'red'
+    static color:string = '#f44336'
     static num:number = 0
     static createShapes(x:number,y:number):Shape {
         var shape:Shape = new Square(x,y)
         switch(ShapeFactory.num %3) {
             case 1:
                shape = new Circle(x,y)
+               break
             case 2:
                shape = new Triangle(x,y)
+               break
+            default:
+               break
         }
         shape.define(ShapeFactory.color)
         ShapeFactory.num++
