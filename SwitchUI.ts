019ff45f3daf2f7ad00a0bf319c3738fd259e42e
Anwesh43 <anweshthecool0@gmail.com>
2017-07-13 23:22:32 +0530

    adding SwitchUI fixes

--- SwitchUI.ts
+++ SwitchUI.ts
@@ -23,19 +23,19 @@ class BasicSwitch {
         context.fillStyle = this.color
         context.save()
         context.beginPath()
-        context.rect(w/20,0,(w/7-w/10)*this.scale,ch)
+        context.rect(0,0,(w/7)*this.scale,ch)
         context.clip()
         this.defineSwitchShape(context,ch)
         context.restore()
         context.beginPath()
-        context.arc((w/7)*(this.scale)+ch/2,ch/2,ch/2,0,2*Math.PI)
+        context.arc((w/7-w/20)*(this.scale)+ch/2,ch/2,ch/2,0,2*Math.PI)
         context.fill()
         this.img.src = canvas.toDataURL()
     }
     defineSwitchShape(context,ch) {
         context.beginPath()
         context.arc(w/20,ch/2,ch/3,Math.PI/2,3*Math.PI/2)
-        context.lineTo(w/7-w/20,-ch/6)
+        context.lineTo(w/7-w/20,ch/6)
         context.arc(w/7-w/20,ch/2,ch/3,3*Math.PI/2,5*Math.PI/2)
         context.lineTo(w/20,5*ch/6)
         context.fill()
