a3cf9b04a0630071f8cd962df8af5ca9f6919bb5
Anwesh43 <anweshthecool0@gmail.com>
2017-07-14 19:22:53 +0530

    adding ShapedImage class

--- shaped-image-color-filter.ts
+++ shaped-image-color-filter.ts
@@ -1,3 +1,4 @@
+const w = window.innerWidth,h = window.innerHeight,size = Math.min(w,h)/5
 class StateContainer {
     scale:number = 0;
     dir:number = 0;
@@ -25,5 +26,32 @@ class StateContainer {
     }
 }
 class ShapedImage {
+    stateContainer:StateContainer = new StateContainer();
+    img:any = document.createElement('img')
+    constructor() {
+        this.initImgElement()
+    }
+    initImgElement() {
+        document.body.appendChild(this.img)
+        this.img.onmousedown = (event) => {
+            this.stateContainer.startUpdating()
+        }
+    }
+    draw(context) {
 
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size
+        const context = canvas.getContext('2d')
+        this.draw(context)
+        this.img.src = canvas.toDataURL()
+    }
+    update() {
+        this.stateContainer.update()
+    }
+    stopped():boolean {
+      return this.stateContainer.stopped()
+    }
 }
